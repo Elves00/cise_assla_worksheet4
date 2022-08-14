50170e50e635b0846b8305c60aaf9a2f3e615c00
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-08-14 12:22:21 +1200

    routing fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from "react";
-import {Router} from "react-router-dom";
+import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
 
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-            <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
-            </ul>
-          <div className="content">
-            
-          </div>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice">Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle">Submit an Article</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/SEPractice" component={SEPractice} />
+          <Route path="/SubmitArticle" component={SubmitArticle} />
         </div>
-        </Router>
-    );
-}
- 
+      </div>
+    </Router>
+  );
+};
+
 export default App;
