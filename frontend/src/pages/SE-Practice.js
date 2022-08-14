6911e1a0ebc8ae48f11ad08c5349b0e0577bcc20
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-08-14 13:52:27 +1200

    Relocating folders to fix imports

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,12 +1,14 @@
-import articles from "../dummydata/articles.js";
+import articles from "../dummydata/article";
 import Styles from "../components/tablestyle.js";
-import Table from "../components/evidencetable.js";
+import Table from "../components/Evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
+import Dropdown from "../components/Dropdown.js";
 
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+              <Dropdown/>
                <Styles>
                  <Table
                   data={articles}
